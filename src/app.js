5d5b78f59af8057bd1ce40ca8909477d17fc1164
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-09-28 18:12:23 +0300

    test changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 function greet(name) {
 
     return `Hello, ${name}!`;
-    
-    }
 
-    module.exports = greet;
+}
 
-    if (require.main === module){
-        console.log(greet("World"))
-    }
+module.exports = greet;
+
+// if (require.main === module) {
+//     console.log(greet("World"))
+// }
